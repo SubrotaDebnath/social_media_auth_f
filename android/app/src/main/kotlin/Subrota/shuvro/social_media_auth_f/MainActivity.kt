75328154a4bb8ece3197cbd914c7a19104110108
Subrota <subrotadebnath1807@gmail.com>
2021-08-31 04:37:42 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/Subrota/shuvro/social_media_auth_f/MainActivity.kt
@@ -0,0 +1,6 @@
+package Subrota.shuvro.social_media_auth_f
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Google Signup'),
+              ),
+              SizedBox(height: 16,),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Facebook Signup'),
+              ),
+              SizedBox(height: 16,),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Apple Signup'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
