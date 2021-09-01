9c47b591da411965aee9b86bba1b66fff0138275
Subrota <subrotadebnath1807@gmail.com>
2021-09-01 20:04:41 +0600

    second page

---
+++ lib/login_sucess_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginSuccess extends StatefulWidget {
+  const LoginSuccess({Key? key}) : super(key: key);
+
+  @override
+  _LoginSuccessState createState() => _LoginSuccessState();
+}
+
+class _LoginSuccessState extends State<LoginSuccess> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
